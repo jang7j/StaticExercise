f26eab9cf78a0e5c0506d058da89f584d2f91421
Yun Jang <jang7j@yahoo.com>
2022-08-22 13:31:11 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,5 @@
+﻿using StaticExercise;
+
+Console.WriteLine(TempConverter.FahrenheitToCelsius(212.0));
+
+Console.WriteLine(TempConverter.CelsiusToFahrenheit(100.0));
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double degrees)
+        {
+            return (degrees - 32.0) * (5.0 / 9.0);
+        }
+        public static double CelsiusToFahrenheit(double degrees)
+        {
+            return (degrees * (9.0 / 5.0)) + 32;
+        }
+
+    }
+}
+
